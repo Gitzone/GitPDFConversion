1cbd6a3471e868fff718f32b459f347ff3665cb5
Gitzone <amitkumardev89@gmail.com>
2014-10-08 08:25:26 +0530

    Final Code

---
+++ Latest Code/8 Oct/PDFConversionAPP/PDFConversionAPP/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PDFConversionAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rootPanel = new System.Windows.Forms.Panel();
+            this.panelFill = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFileNames = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rootPanel.SuspendLayout();
+            this.panelFill.SuspendLayout();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rootPanel
+            // 
+            this.rootPanel.Controls.Add(this.panelFill);
+            this.rootPanel.Controls.Add(this.topPanel);
+            this.rootPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rootPanel.Location = new System.Drawing.Point(0, 0);
+            this.rootPanel.Name = "rootPanel";
+            this.rootPanel.Size = new System.Drawing.Size(498, 282);
+            this.rootPanel.TabIndex = 0;
+            // 
+            // panelFill
+            // 
+            this.panelFill.BackColor = System.Drawing.Color.Linen;
+            this.panelFill.Controls.Add(this.progressBar1);
+            this.panelFill.Controls.Add(this.label1);
+            this.panelFill.Controls.Add(this.lblFileNames);
+            this.panelFill.Controls.Add(this.btnConvert);
+            this.panelFill.Controls.Add(this.btnBrowse);
+            this.panelFill.Controls.Add(this.lblSelect);
+            this.panelFill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFill.Location = new System.Drawing.Point(0, 54);
+            this.panelFill.Name = "panelFill";
+            this.panelFill.Size = new System.Drawing.Size(498, 228);
+            this.panelFill.TabIndex = 1;
+            this.panelFill.Paint += new System.Windows.Forms.PaintEventHandler(this.panelFill_Paint);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(125, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(271, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(12, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Note: Only JEPG,JPG,TEXT,GIF,DOC,DOCX,XLSX,XLS, File formats are accepted.";
+            // 
+            // lblFileNames
+            // 
+            this.lblFileNames.AutoSize = true;
+            this.lblFileNames.Location = new System.Drawing.Point(210, 86);
+            this.lblFileNames.Name = "lblFileNames";
+            this.lblFileNames.Size = new System.Drawing.Size(54, 13);
+            this.lblFileNames.TabIndex = 4;
+            this.lblFileNames.Text = "File Name";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(213, 152);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(213, 43);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelect.Location = new System.Drawing.Point(76, 46);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(99, 17);
+            this.lblSelect.TabIndex = 0;
+            this.lblSelect.Text = "Select Files:";
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.Linen;
+            this.topPanel.Controls.Add(this.lblHeading);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(498, 54);
+            this.topPanel.TabIndex = 0;
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(121, 20);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(275, 20);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Upload Files For PDF Conversion";
+            this.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(498, 282);
+            this.Controls.Add(this.rootPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Conversion Window";
+            this.rootPanel.ResumeLayout(false);
+            this.panelFill.ResumeLayout(false);
+            this.panelFill.PerformLayout();
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel rootPanel;
+        private System.Windows.Forms.Panel panelFill;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblFileNames;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Latest Code/8 Oct/PDFConversionAPP/PDFConversionAPP/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Conversion;
+using PdfSharp.Pdf;
+using PDFConversionAPP.ServiceReference1;
+using System.ServiceModel;
+
+namespace PDFConversionAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Filter =
+"CSV Files (*.csv)|*.csv|" +
+"Excel Files (*.xls)|*.xls";
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                lblFileNames.Text = openFileDialog1.FileName;
+
+            }
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            /**Progress Bar***/
+            progressBar1.Enabled = true;
+            progressBar1.Minimum = 1;
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 10;
+            progressBar1.PerformStep();
+            /**End**/
+
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.MaxBufferPoolSize = 2147483647;
+            binding.MaxBufferSize = 2147483647;
+            binding.MaxReceivedMessageSize = 2147483647;
+            string url = System.Configuration.ConfigurationSettings.AppSettings["webserviceURL"];
+            EndpointAddress address = new EndpointAddress(url);
+
+            Service1SoapClient sc = new Service1SoapClient(binding, address);
+           
+            
+
+
+            PdfDocument doc = new PdfDocument();
+            int count = 0;
+            string[] FilenameName;
+            /***Final Output file folder***/
+            string appPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string exportPath = appPath + "\\Uploads";
+            bool exists = System.IO.Directory.Exists(exportPath);
+            if (!exists)
+                System.IO.Directory.CreateDirectory(exportPath);
+
+            /****End***/
+
+           
+            progressBar1.PerformStep();
+
+            foreach (string item in openFileDialog1.FileNames)
+            {
+
+                FilenameName = item.Split('\\');
+                string extension = Path.GetExtension(FilenameName[FilenameName.Length - 1]).ToLower();
+                string fullfilename = Path.GetFileName(FilenameName[FilenameName.Length - 1]);
+                string[] filename = fullfilename.Split('.');
+                if (extension == ".jpg" || extension == ".jpeg" || extension == ".gif")
+                {
+                   
+                    ConvertFileToStream("IMAGE", item);
+                }
+                else if (extension == ".xls" || extension == ".xlsx")
+                {
+                    ConvertFileToStream("EXCEL",item);
+                }
+                else if (extension == ".txt")
+                {
+                    ConvertFileToStream("TEXT", item);
+                }
+                else if (extension == ".doc" || extension == ".docx")
+                {
+                    ConvertFileToStream("DOC", item);
+                }
+                count++;
+                progressBar1.PerformStep();
+            }
+            progressBar1.PerformStep();
+            Conversion.PDFConversion con = new PDFConversion();
+           
+
+           var documentContents=  sc.MergDocuments();
+           saveFinalDocuments(documentContents, exportPath);
+        
+            progressBar1.Value = 100;
+            progressBar1.Enabled = false;
+            progressBar1.PerformStep();
+            MessageBox.Show("File Created on location:" + exportPath);
+
+        }
+
+        private void ConvertFileToStream(string docType, string sFile)
+        {
+            Service1SoapClient sc = new Service1SoapClient();
+            FileStream objfilestream = new FileStream(sFile, FileMode.Open, FileAccess.Read);
+            int len = (int)objfilestream.Length;
+            Byte[] mybytearray = new Byte[len];
+            objfilestream.Read(mybytearray, 0, len);
+            sc.SaveDocument(mybytearray, sFile.Remove(0, sFile.LastIndexOf("\\") + 1), docType);
+            objfilestream.Close();
+        }
+
+        private void saveFinalDocuments(byte[] documentContents,string exportPath)
+        {
+            string datetimeString = string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}.txt", DateTime.Now);
+            string sFile = exportPath + "\\" + datetimeString+".pdf";
+            MemoryStream objstreaminput = new MemoryStream();
+            FileStream objfilestream = new FileStream(sFile.Insert(sFile.LastIndexOf("."), "2"), FileMode.Create, FileAccess.ReadWrite);
+
+           
+            int len = documentContents.Length;   
+            Byte[] mybytearray = new Byte[len];
+            objfilestream.Write(documentContents, 0, len);
+            objfilestream.Close();
+        }
+
+        private void panelFill_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
